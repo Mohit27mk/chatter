5ff609830e59976ae67267367132d79e19e9f3ac
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-02-28 17:59:20 +0530

    signup frontend done

---
+++ public/signup/signup.js
@@ -0,0 +1,39 @@
+const signupForm=document.querySelector('#signup-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const phonenoInput = document.querySelector('#phoneno');
+const passwordInput = document.querySelector('#password');
+
+signupForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  const name=e.target.name.value;
+  const email=e.target.email.value;
+  const phoneno=e.target.phoneno.value;
+  const password=e.target.password.value;
+
+    const myobj={
+      name,email,phoneno,password
+    }
+    
+    axios.post("http://localhost:3000/user/signup",myobj)
+    .then((res)=>{
+      if(res.data.Email==='exist'){
+        alert("Email already exist");
+      }
+      if(res.data.Phoneno==='exist'){
+        alert("Phoneno already exist");
+      }
+        window.location.href='./login.html';
+      
+    }).catch((err)=>{
+     console.log(err);
+    })
+   nameInput.value='';
+   emailInput.value='';
+   phonenoInput.value='';
+   passwordInput.value=''; 
+   
+}
